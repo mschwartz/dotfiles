a4b804fff02d12cfab6363bc2817c2755fc2f858
mschwartz <mykesx@gmail.com>
2016-10-13 07:43:28 -0700

    umount snowdog

--- bin/mount-showdog.sh
+++ bin/mount-showdog.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
+OPTIONS=reconnect,follow_symlinks
 mkdir -p $HOME/snowdog/github
-mkdir-p $HOME/snowdog/projects
+mkdir -p $HOME/snowdog/projects
 
-sshfs mschwartz@snowdog:sencha/github $HOME/snowdog/github -ocache=no,nolocalcache,volname=snowdog,defer_permissions,reconnect
-sshfs mschwartz@snowdog:sencha/projects $HOME/snowdog/projects -ocache=no,nolocalcache,volname=snowdog,defer_permissions,reconnect
+sshfs mschwartz@snowdog:sencha/github $HOME/snowdog/github -o$OPTIONS -ovolname="github"
+sshfs mschwartz@snowdog:sencha/projects $HOME/snowdog/projects -o$OPTIONS -ovolname="projects"
 
