02050d41bd7041af1f04592aabb349b684c4093a
Michael Schwartz <mykesx@gmail.com>
2017-03-16 16:06:22 -0700

    Set tmux pane title on ssh connections

--- functions.zsh
+++ functions.zsh
@@ -29,3 +29,14 @@ issue () {
     mv ext xxx
     ln -s ../../github/SDK6.5/ext ext
 }
+
+ssh() {
+    if [ "$(ps -p $(ps -p $$ -o ppid=) -o comm=)" = "tmux" ]; then
+        tmux rename-window "$(echo $* | cut -d . -f 1)"
+        command ssh "$@"
+        tmux set-window-option automatic-rename "on" 1>/dev/null
+    else
+        command ssh "$@"
+    fi
+}
+
