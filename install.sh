61742270b8ed8c40a88d37833aa35870a8d32b45
Michael Schwartz <mykesx@gmail.com>
2017-05-18 08:04:27 -0700

    fix install

--- install.sh
+++ install.sh
@@ -72,7 +72,7 @@ npm install -g jsctags
 git submodule update --init --recursive
 cd bundle/tern_for_vim
 npm install
-if [[ "$platform" == 'macos ']]; then
+if [[ "$platform" == "macos" ]]; then
     cd ../YouCompleteMe
     if [ "$type" = "armv71" ]; then
         export YCM_CORES=1
