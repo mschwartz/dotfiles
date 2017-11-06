a3e4a54c41f91bfb857e6ab933eb0168a995b26b
Michael Schwartz <mykesx@gmail.com>
2017-11-06 10:12:51 -0800

    add screenfetch

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ files="path profile bash_profile bashrc bash_aliases vimrc vim zlogin zshrc zpro
 
 echo "installing prerequisites $platform"
 if [[ $platform == 'linux' ]]; then
-    sudo apt-get install -y build-essential speedtest-cli zsh vim cmake vim-youcompleteme python-dev libboost-all-dev tmux curl ruby-dev
+    sudo apt-get install -y build-essential speedtest-cli zsh vim cmake vim-youcompleteme python-dev libboost-all-dev tmux curl ruby-dev screenfetch
     echo ...nodejs
     curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
     sudo apt-get update
@@ -31,6 +31,7 @@ if [[ $platform == 'linux' ]]; then
     chsh -s $(which zsh)
 elif [[ $platform == 'macos' ]]; then
     echo ...nodejs
+    brew install screenfetch
     # brew install nodejs etc
 fi
 
