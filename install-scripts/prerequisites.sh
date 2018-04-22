fe423962bc128c123647d7b7331417af5a22cfab
Michael Schwartz <mykesx@gmail.com>
2018-04-22 14:43:18 -0700

    assorted fixes, and adding install-scripts

---
+++ install-scripts/prerequisites.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+echo "installing prerequisites $platform"
+if [[ $platform == 'linux' ]]; then
+  PACKAGES_TO_INSTALL="\
+    build-essential \
+    speedtest-cli \
+    source-highlight \
+    zsh \
+    vim-nox \
+    cmake \
+    vim-youcompleteme \
+    python-dev \
+    libboost-all-dev \
+    tmux \
+    curl \
+    ruby-dev \
+    ctags \
+    screenfetch \
+    net-tools \
+    neofetch \
+    fonts-font-awesome \
+    urlview
+    "
+
+  sudo apt-get install -y $PACKAGES_TO_INSTALL
+  echo ...nodejs
+  curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
+  sudo apt-get update
+  sudo apt-get -y dist-upgrade
+  sudo apt-get install -y nodejs
+  npm config set prefix '/usr/local'
+  sudo mkdir -p $(whoami) $(npm config get prefix)/{lib/node_modules,bin,share}
+  sudo chown -R $(whoami) $(npm config get prefix)/{lib/node_modules,bin,share}
+  sudo chown -R $(whoami) /usr/local/share/zsh/site-functions
+  chsh -s $(which zsh)
+elif [[ $platform == 'macos' ]]; then
+  echo ...nodejs
+  brew install neofetch
+  # brew install nodejs etc
+fi
+
