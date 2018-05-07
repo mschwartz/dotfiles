2260a2fdac9bcf27139a8ff54e7b2fa7bfbc91d0
Michael Schwartz <mykesx@gmail.com>
2018-05-07 07:04:13 -0700

    fix for osx

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+INSTALL_PWD=`pwd`
 ########## Variables
 . ./install-scripts/lib/platform.sh
 source ./install-scripts/prerequisites.sh
@@ -33,5 +34,7 @@ npm install
 # oh my zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
+echo cd $INSTALL_PWD
+cd $INSTALL_PWD
 source ./install-scripts/install-docker.sh
 
