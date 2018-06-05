ba1373d7b05f12fda930953a42862d00d7eaf793
Michael Schwartz <mykesx@gmail.com>
2018-06-05 10:33:44 -0700

    use edge version of docker for 18.04

--- install-scripts/install-docker.sh
+++ install-scripts/install-docker.sh
@@ -15,7 +15,7 @@ if [[ $platform == 'linux' ]]; then
   sudo add-apt-repository \
     "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
     $(lsb_release -cs) \
-    stable"
+    edge"
   sudo apt-get update
   sudo apt-get install -y docker-ce
   sudo apt-get install -y python-pip
