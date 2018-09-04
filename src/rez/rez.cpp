93a58dffce0d4a197801a58ef295c15a7cfe3f03
Michael Schwartz <mykesx@gmail.com>
2018-09-03 19:09:24 -0700

    tweaks to resolution setting

--- src/rez/rez.cpp
+++ src/rez/rez.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 
@@ -13,11 +14,22 @@ int main(int ac, char *av[]) {
     line[nread-1] = '\0';
     char *ptr = line;
     while (*ptr != '\0' && isspace(*ptr)) *ptr++;
+    int width = 0;
     if (*ptr >= '0' && *ptr <= '9') {
       char *end = ptr;
-      while (!isspace(*end) && *end != '\0') end++;
-      char *mode = strndup(ptr, end-ptr);
-      printf("%s\n", mode);
+      while (isdigit(*end) && *end != '\0') {
+        width = width * 10 + (*end - '0');
+        end++;
+      }
+      while (!isspace(*end) && *end != '\0') {
+        end++;
+      }
+      if (width >= 1024) {
+        char *mode = strndup(ptr, end-ptr);
+        printf("%s\n", mode);
+//        printf("%d %s\n", width, mode);
+        free(mode);
+      }
     }
 //    printf("%5d %5d %s\n", len, nread, line);
   } 
