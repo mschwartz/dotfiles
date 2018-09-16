4e82f4e6039d5fa33f984497d2852b4e4991a947
Michael Schwartz <mykesx@gail.com>
2018-09-16 14:16:43 -0700

    install more stuff for arch

--- install-i3.sh
+++ install-i3.sh
@@ -10,6 +10,10 @@ if [[ $platform == 'linux' ]]; then
   sudo apt-get install -y libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm0 libxcb-xrm-dev automake
 fi
 
+if [[ $platform == 'arch' ]]; then
+sudo pacman -S i3blocks i3status
+fi
+
 # i3 config files
 mkdir -p ~/.config
 mkdir -p ~/dotfiles_old/config
