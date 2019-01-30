68577c1f687c14551eefbdb7dae68556d5e52f06
Michael Schwartz <mykesx@gmail.com>
2019-01-29 19:44:55 -0800

    remove debugging

--- config/i3/toggle-trackpad.sh
+++ config/i3/toggle-trackpad.sh
@@ -4,14 +4,12 @@ tp=`xinput list --name-only | grep -i TouchPad`
 TPadState=`xinput --list-props "$tp"|grep "Device Enabled"|tail -c -2`
 #MouseState=`xinput --list-props "$mouse"|grep "Device Enabled"|tail -c -2`
 
-echo $TPadState
 if [ "$TPadState" = "1" ];
 then
         `xinput --set-prop "$tp" "Device Enabled" 0`
 	TPadState+=$(xinput --list-props "$tp"|grep "Device Enabled"|tail -c -2)
         notify-send -t 2000 "TouchPad disabled!"
 else
-  echo "here"
         `xinput --set-prop "$tp" "Device Enabled" 1`
 	TPadState+=$(xinput --list-props "$tp"|grep "Device Enabled"|tail -c -2)
         notify-send -t 2000 "TouchPad enabled!"
