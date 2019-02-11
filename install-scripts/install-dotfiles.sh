71052d38cacdd076112b5462a45fa376ea983833
Michael Schwartz <mykesx@gmail.com>
2019-02-10 19:40:26 -0800

    backup to .dotfiles_old

--- install-scripts/install-dotfiles.sh
+++ install-scripts/install-dotfiles.sh
@@ -4,13 +4,13 @@ echo ">>> INSTALLING DOTFILES"
 
 # create dotfiles_old in homedir
 dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
+olddir=~/.dotfiles_old             # old dotfiles backup directory
 files="XResources inputrc profile bash_profile bashrc bash_aliases vimrc vim zlogin zshrc zprofile zshenv Xresources ctags tmux tmux.conf ackrc ideavimrc agignore eslintrc.js clang-format"    # list of files/folders to symlink in homedir
 
-#echo "Creating $olddir for backup of any existing dotfiles in ~"
-#mkdir -p $olddir
-#rm -rf $olddir/*
-#echo "...done"
+echo "Creating $olddir for backup of any existing dotfiles in ~"
+mkdir -p $olddir
+rm -rf $olddir/*
+echo "...done"
 
 # change to the dotfiles directory
 echo "Changing to the $dir directory"
@@ -18,10 +18,11 @@ cd $dir
 echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+echo "Moving any existing dotfiles from ~ to $olddir"
 for file in $files; do
-  #echo "Moving any existing dotfiles from ~ to $olddir"
-  #echo mv ~/.$file $olddir/.$file
-  #mv ~/.$file $olddir/.$file
+  echo mv ~/.$file $olddir/.$file
+  rm -f $olddir/.$file
+  mv ~/.$file $olddir/.$file
   echo "Creating symlink to $file in home directory."
   echo rm -f ~/.$file
   rm -f ~/.$file
