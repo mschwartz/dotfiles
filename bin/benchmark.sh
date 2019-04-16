7c541da5654b93f6dad7c0aba68c2ba2a010f08f
Michael Schwartz <mykesx@gmail.com>
2019-04-16 16:33:05 -0700

    tail -1 dd output

--- bin/benchmark.sh
+++ bin/benchmark.sh
@@ -3,14 +3,17 @@
 mkdir -p ~/tmp
 cd ~/tmp
 echo "=========> DD disk write"
-dd if=/dev/zero of=tempfile bs=1M count=1024 conv=fdatasync,notrunc status=progress | tail -1 2>&1
+dd if=/dev/zero of=tempfile bs=1M count=1024 conv=fdatasync,notrunc status=progress >out 2>&1
+tail -1 out
 echo "=========> DD disk read"
 sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
-dd if=tempfile of=/dev/null bs=1M count=1024 status=progress | tail -1 2>&1
+dd if=tempfile of=/dev/null bs=1M count=1024 status=progress >out 2>&1
+tail -1 out
 echo "=========> DD disk cache read"
-dd if=tempfile of=/dev/null bs=1M count=1024 status=progress | tail -1 2>&1
+dd if=tempfile of=/dev/null bs=1M count=1024 status=progress >out 2>&1
+tail -1 out
 
-rm ~/tmp/tempfile
+rm ~/tmp/tempfile out
 
 #echo "=========> HDPARM"
 #hdparm -Tt /dev/nvme0n1
