f8f315b4a268b381b3c7ae3e07b5b6bfcce37bb1
Michael Schwartz <mykesx@gmail.com>
2019-04-16 17:19:39 -0700

    /bin/echo

--- bin/benchmark.sh
+++ bin/benchmark.sh
@@ -6,7 +6,7 @@ echo "=========> DD disk write"
 dd if=/dev/zero of=tempfile bs=1M count=1024 conv=fdatasync,notrunc status=progress >out 2>&1
 tail -1 out
 echo "=========> DD disk read"
-sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
+sudo sh -c "/bin/echo 3 > /proc/sys/vm/drop_caches"
 dd if=tempfile of=/dev/null bs=1M count=1024 status=progress >out 2>&1
 tail -1 out
 echo "=========> DD disk cache read"
