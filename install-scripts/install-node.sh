bdd68e757d3b699d091adac501aa2cf48fdd527b
Michael Schwartz <mykesx@gmail.com>
2019-04-24 11:28:40 -0700

    fix typo

--- install-scripts/install-node.sh
+++ install-scripts/install-node.sh
@@ -11,7 +11,7 @@ if [[ $platform == 'linux' ]]; then
   sudo apt-get install -y nodejs
 elif [[ $platform == 'arch' ]]; then
   sudo pacman -S --noconfirm nodejs npm
-  yay -S --noconfigm nvm
+  yay -S --noconfirm nvm
 elif [[ $platform == 'macos' ]]; then
   install node
 fi
