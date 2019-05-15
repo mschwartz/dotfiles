b6316f9c2e1be633992c970ad11af527878bd812
Michael Schwartz <mykesx@gmail.com>
2019-05-15 11:11:04 -0700

    remove debugging

--- install-scripts/install-node.sh
+++ install-scripts/install-node.sh
@@ -20,11 +20,7 @@ npm config set prefix '/usr/local'
 sudo mkdir -p $(whoami) $(npm config get prefix)/{lib/node_modules,bin,share}
 sudo chown -R $(whoami) $(npm config get prefix)/{lib/node_modules,bin,share}
 
-echo "1"
 npm install -g eslint eslint-plugin-class-property eslint-plugin-jest eslint-plugin-react jsctags react-native-cli prettier webpack babel-cli babel-eslint babel-plugin-transform-es2015-modules-commonjs babel-preset-stage-0 babel-preset-stage-2 create-react-app
-echo "2"
 npm install -g eslint@4.6.0 
-echo "3"
 npm install -g typescript-eslint-parser
-echo "4"
 npm install -g eslint-plugin-prettier
