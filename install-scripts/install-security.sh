de3fffdea408ac6f63d9cda888bff568cafe0353
Michael Schwartz <mykesx@gmail.com>
2019-05-31 12:27:20 -0700

    add git secrets for macos

--- install-scripts/install-security.sh
+++ install-scripts/install-security.sh
@@ -6,5 +6,7 @@ if [[ $platform == 'arch' ]]; then
   sudo pacman --noconfirm -S intel-ucode
   sudo grub-mkconfig -o /boot/grub/grub.cfg
   yay --noconfirm -Syy git-secrets
+elif [[ $platform == 'macos' ]]; then
+  brew install git-secrets
 fi
 
