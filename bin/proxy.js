2b16ff83b659459e77f8e22378f7fb2a7185575a
Michael Schwartz <mykesx@gmail.com>
2019-05-31 12:29:32 -0700

    remove js progs from bin

--- bin/proxy.js
+++
@@ -1,8 +0,0 @@
-var http = require('http'),
-    httpProxy = require('http-proxy');
-
-//
-// Create your proxy server and set the target in the options.
-//
-httpProxy.createProxyServer({target:'http://snowdog:8080'}).listen(8000); // See (†)
-
