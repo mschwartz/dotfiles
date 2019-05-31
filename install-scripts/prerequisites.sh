3e7ab5424a0799f8bbf28342aa6ac31eacf9e3c4
Michael Schwartz <mykesx@gmail.com>
2019-05-31 13:33:36 -0700

    figlet

--- install-scripts/prerequisites.sh
+++ install-scripts/prerequisites.sh
@@ -26,7 +26,7 @@ if [[ $platform == 'linux' ]]; then
   "
 
   sudo apt-get install figlet
-  figlet Prerequisites
+  figlet "Prerequisites for $platform"
 
   sudo apt-get update
   sudo apt-get -y dist-upgrade
@@ -60,7 +60,9 @@ elif [[ $platform == 'arch' ]]; then
     hub \
     "
 
-  sudo pacman -Syy
+  sudo pacman -Syy figlet
+  figlet "Prerequisites for $platform"
+
   # uncomment this if keys are out of date - it is really slow
 #  sudo pacman-key --refresh-keys
   gpg --recv-keys 1C61A2656FB57B7E4DE0F4C1FC918B335044912E
