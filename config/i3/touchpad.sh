d512e7eefdae867e03fcf41698514e5e2f0430bd
mschwartz <mykesx@gmail.com>
2019-08-11 14:52:00 -0700

    do touchpad config dynamically in touchpad.sh (from config

--- config/i3/touchpad.sh
+++ config/i3/touchpad.sh
@@ -4,4 +4,17 @@
 if [ -e /usr/bin/synclient ]; then
   synclient TapButton1=0
   synclient TapButton2=0
+  synclient VertScrollDelta=-113
+  synclient PalmDetect=1
+else
+  touchpad=`xinput list --name-only | grep -i TouchPad`
+  trackpoint=`xinput list --name-only | grep -i TrackPoint`
+
+  xinput --set-prop $touchpad 'libinput Accel Speed' .5
+  xinput --set-prop $touchpad 'libinput Accel Speed Default' .5
+  xinput --set-prop $touchpad 'libinput Natural Scrolling Enabled' 1
+  xinput --set-prop $touchpad 'libinput Natural Scrolling Enabled Default' 1
+  xinput --set-prop $touchpad 'libinput Tapping Enabled' 0
+  xinput --set-prop $touchpad 'libinput Tapping Enabled Default' 0
+  xinput --set-prop $trackpoint 'Device Enabled' 0
 fi
