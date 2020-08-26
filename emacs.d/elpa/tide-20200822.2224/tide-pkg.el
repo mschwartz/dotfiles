c2ec4304c189b4fafe88cb952606212d9acfc679
Michael Schwartz <mykesx@gmail.com>
2020-08-26 14:25:32 -0700

    typescript

---
+++ emacs.d/elpa/tide-20200822.2224/tide-pkg.el
@@ -0,0 +1,17 @@
+(define-package "tide" "20200822.2224" "Typescript Interactive Development Environment"
+  '((emacs "25.1")
+    (dash "2.10.0")
+    (s "1.11.0")
+    (flycheck "27")
+    (typescript-mode "0.1")
+    (cl-lib "0.5"))
+  :keywords
+  '("typescript")
+  :authors
+  '(("Anantha kumaran" . "ananthakumaran@gmail.com"))
+  :maintainer
+  '("Anantha kumaran" . "ananthakumaran@gmail.com")
+  :url "http://github.com/ananthakumaran/tide")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
