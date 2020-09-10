eb537e5c1812b07527bbff69774613a122db861f
Michael Schwartz <mykesx@gmail.com>
2020-09-09 18:09:36 -0700

    fix install-emacs

--- install-scripts/install-emacs.sh
+++ install-scripts/install-emacs.sh
@@ -5,7 +5,7 @@ figlet "Install emacs"
 . ~/dotfiles/install-scripts/lib/platform.sh
 
 if [[ $platform == 'linux' ]]; then
-  sudo apt-get install -y emacs26
+  sudo apt-get install -y emacs
 elif [[ $platform == 'arch' ]]; then
     sudo pacman -S --noconfirm emacs
 else # macos
@@ -13,25 +13,17 @@ else # macos
 fi
 
 mkdir -p ~/github/other
-#<<<<<<< HEAD
 export OTHER="$HOME/github/other";
 export ICONS_IN_TERMINAL="$OTHER/icons-in-terminal";
-
 echo "  installing icons-in-terminal"
-if [ ! -d $ICONS_IN_TEMRMINAL ]; then
+
+if [ -d "$ICONS_IN_TERMINAL" ]; then
+  echo "$ICONS_IN_TERMINAL exists"
+else
   echo "$ICONS_IN_TEMRMINAL does not exist"
   cd $OTHER
-  git clone git@:github.com/sebastiencs/icons-in-terminal.git
-else
-  echo "$ICONS_IN_TEMRMINAL exists"
+  git clone git@github.com:sebastiencs/icons-in-terminal.git
 fi
-cd $HOME/github/other/icons-in-terminal
-./install-autodetect.sh || ./install.sh
-#=======
-#pushd ~/github/other
-git clone https://github.com/sebastiencs/icons-in-terminal.git
 
-#git clone git@github.com:sebastiencs/icons-in-terminal.git
-#cd icons-in-terminal
-#./install-autodetect.sh
-#>>>>>>> 6dfef233e8de0a2bad4665e2f0bedfe6cff523a5
+cd $ICONS_IN_TERMINAL
+./install-autodetect.sh || ./install.sh
