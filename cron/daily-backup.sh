706610a50973a31980ea5085f2a5bc242bd80e33
Michael Schwartz <mykesx@gmail.com>
2021-02-07 13:34:31 -0800

    backups

--- cron/daily-backup.sh
+++ cron/daily-backup.sh
@@ -14,12 +14,12 @@ echo "DAILY BACKUP"
 sudo mount /backup
 sudo mkdir -p /backup/$HOSTNAME/daily
 echo "*** Backing up /etc"
-sudo rsync -avO --delete /etc/ /backup/$HOSTNAME/daily/etc
+sudo rsync -azvO --exclude node_modules --exclude .cache --delete /etc/ /backup/$HOSTNAME/daily/etc
 echo "*** Backing up /opt"
-sudo rsync -avO --delete /opt/ /backup/$HOSTNAME/daily/opt
+sudo rsync -azvO --exclude node_modules --exclude .cache --delete /opt/ /backup/$HOSTNAME/daily/opt
 echo "*** Backing up /var"
-sudo rsync -avO --delete /var/ /backup/$HOSTNAME/daily/var
+sudo rsync -azvO --exclude node_modules --exclude .cache --delete /var/ /backup/$HOSTNAME/daily/var
 echo "*** Backing up /usr/local"
-sudo rsync -avO --delete /usr/local/ /backup/$HOSTNAME/daily/usr.local
+sudo rsync -azvO --exclude node_modules --exclude .cache --delete /usr/local/ /backup/$HOSTNAME/daily/usr.local
 echo "*** Backing up /home"
-sudo rsync -avO --delete /home/ /backup/$HOSTNAME/daily/home
+sudo rsync -azvO --exclude node_modules --exclude .cache --delete /home/ /backup/$HOSTNAME/daily/home
