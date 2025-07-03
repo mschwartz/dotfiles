c73c06f7d1008bca6124405fb3b3687e67feca11
Michael Schwartz <mykesx@gmail.com>
2025-07-02 19:13:45 -0700

    bad eza

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -104,7 +104,7 @@ alias ez='vi ~/.zshrc.local ~/.zshrc'
 alias root='cd `git rev-parse --show-toplevel`'
 alias modus='cd $HOME/github/ModusCreateOrg'
 
-alias ls='eza'
-alias ll='eza -l'
-alias la='eza -la'
+#alias ls='eza'
+alias ll='ls -l'
+alias la='ls -la'
 
